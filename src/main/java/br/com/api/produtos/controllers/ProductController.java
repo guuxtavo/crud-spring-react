2973738655811162772e65f2850f591165f2977e
Gustavo Nestor <gustavonestor1010@gmail.com>
2023-07-06 15:31:15 -0300

    Implementacao do metodo cadastrar

--- src/main/java/br/com/api/produtos/controllers/ProductController.java
+++ src/main/java/br/com/api/produtos/controllers/ProductController.java
@@ -1,14 +1,35 @@
 package br.com.api.produtos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.models.Product;
+import br.com.api.produtos.services.ProductService;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody Product p){
+        return service.cadastrar(p);
+    }
+
+
+    @GetMapping("/listar")
+    public Iterable<Product> listar(){
+        return service.listar();
+    }
     
-    @GetMapping
+    @GetMapping("/")
     public String rota(){
         return "api de produtos funcionando";
     }
--- src/main/java/br/com/api/produtos/services/ProductService.java
+++ src/main/java/br/com/api/produtos/services/ProductService.java
@@ -1,11 +1,12 @@
 package br.com.api.produtos.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.api.produtos.models.Product;
+import br.com.api.produtos.models.ResponseModel;
 import br.com.api.produtos.repositories.ProductRepository;
 
 @Service
@@ -14,9 +15,28 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Autowired
+    private ResponseModel response;
+
     // Método para listar todos os produtos
-    public List<Product> listar(){
+    public Iterable<Product> listar() {
         return repository.findAll();
     }
-    
+
+    // Método para cadastrar produtos
+
+    public ResponseEntity<?> cadastrar(Product p) {
+        if (p.getName().equals("")) {
+            response.setMessage("O nome do produto é obrigatório!");
+            return new ResponseEntity<ResponseModel>(response, HttpStatus.BAD_REQUEST);
+        } else if(p.getBrand().equals("")){
+            response.setMessage("O nome da marca é obrigatório");
+            return new ResponseEntity<ResponseModel>(response, HttpStatus.BAD_REQUEST);
+        }else{
+            return new ResponseEntity<Product>(repository.save(p), HttpStatus.CREATED);
+            // Product product = repository.save(p);
+            // return ResponseEntity.ok(product);
+        }
+    }
+
 }
