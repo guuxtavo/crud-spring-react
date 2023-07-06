7aa27b66dd31a9e15cb86746b58234466c670505
Gustavo Nestor <gustavonestor1010@gmail.com>
2023-07-06 15:06:42 -0300

    Implementando metodo listar no ProductService

--- src/main/java/br/com/api/produtos/services/ProductService.java
+++ src/main/java/br/com/api/produtos/services/ProductService.java
@@ -1,5 +1,22 @@
 package br.com.api.produtos.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.models.Product;
+import br.com.api.produtos.repositories.ProductRepository;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    // Método para listar todos os produtos
+    public List<Product> listar(){
+        return repository.findAll();
+    }
     
 }
